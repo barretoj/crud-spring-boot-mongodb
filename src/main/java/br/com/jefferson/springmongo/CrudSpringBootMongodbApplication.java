a6a926ed2d9d7be6c92947b65c3a0f6d1b6dce1d
Jefferson Fernandes Barreto <jeffinho_fb@hotmail.com>
2020-10-19 16:00:47 -0300

    First commit - created project

---
+++ src/main/java/br/com/jefferson/springmongo/CrudSpringBootMongodbApplication.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.springmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringBootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringBootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jefferson/springmongo/CrudSpringBootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jefferson.springmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringBootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
