e5a39b70e9c2b501d9820497e140a1afd4933b08
Jefferson Fernandes Barreto <jeffinho_fb@hotmail.com>
2020-10-20 17:29:02 -0300

    Obtendo um usuário por id

--- src/main/java/br/com/jefferson/springmongo/services/UserService.java
+++ src/main/java/br/com/jefferson/springmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package br.com.jefferson.springmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.jefferson.springmongo.domain.User;
 import br.com.jefferson.springmongo.repository.UserRepository;
+import br.com.jefferson.springmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
+
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
